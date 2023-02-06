edc06695a8c71c0eef830b6109642f3ab0388275
Juliana Rodrigues Nogueira <114073784+rodrigues28ju@users.noreply.github.com>
2023-02-06 08:58:46 -0300

    Delete ResiliaProjGrpMod4-main directory

--- ResiliaProjGrpMod4-main/script perguntas/Quantos circuitos foram realizados nos Estados Unidos e qual e o nome e a localização dos circuitos.sql
+++
@@ -1,4 +0,0 @@
--- Quantos circuitos foram realizados nos Estados Unidos e qual e o nome e a localização dos circuitos
-
-Select name AS "Nome" , location AS "Cidade" FROM circuits
-WHERE country LIKE("%USA%");
